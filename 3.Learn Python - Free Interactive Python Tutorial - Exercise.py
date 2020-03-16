59c2a07068015ea96a631dc2834e505bf13270dd
Cinthya Blois <31311046+ciblois@users.noreply.github.com>
2020-03-16 23:19:55 +0000

    Add files via upload

---
+++ 3.Learn Python - Free Interactive Python Tutorial - Exercise.py
@@ -0,0 +1,19 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #Exercise 3
+>>> numbers = []
+>>> strings = []
+>>> names = ["John", "Eric", "Jessica"]
+>>> second_name = "Eric"
+>>> numbers.append(1)
+>>> numbers.append(2)
+>>> numbers.append(3)
+>>> strings.append("hello")
+>>> strings.append("world")
+>>> print(numbers)
+[1, 2, 3]
+>>> print(strings)
+['hello', 'world']
+>>> print("The second name on the names list is %s" % second_name)
+The second name on the names list is Eric
+>>> 
